375ec5f3927f930c1ad53bc460d1082c21026499
Babel <heavelyn1999@gmail.com>
2021-06-24 13:41:07 +0200

    bonus features

--- index.js
+++ index.js
@@ -67,6 +67,32 @@ class InteractWith {
     }
   }
 
+  async replyTag(msg, replyToArray, replyHowArray) {
+    const matchingRegexArray = replyToArray.find(r => ` ${msg.content} `.match(r));
+    if(matchingRegexArray) {
+      const match = ` ${msg.content} `.match(matchingRegexArray);
+      console.log(match[1]);
+      if (match) {
+        let randomReply = replyHowArray[Math.floor(Math.random()*replyHowArray.length)];
+        for (let item of randomReply) {
+          const personTag = msg.content.replace(match[1], '');
+          console.log(match[1][0]);
+          if (match[1][0] === "!" && msg.content.indexOf(match[1]) === 0 && personTag) {
+            item = item
+              .replace('$person$', personTag)
+          } else {
+            item = item.replace('$person$', msg.author.username)
+          }
+          item = item[0].toUpperCase() + item.substr(1);
+          msg.channel.startTyping();
+          await sleep(1600);
+          msg.channel.send(item);
+          msg.channel.stopTyping();
+        }
+      }
+    }
+  }
+
 //react by using an emoticon, picks random reaction from reactions array, reacts with it.
   react(msg, reactToArray, reactHowArray) {
     const matchingRegexArray = reactToArray.find(r => ` ${msg.content} `.match(r));
@@ -109,11 +135,17 @@ const sexQueryDeclared = [...reactReplyTo.sexListPl, ...reactReplyTo.sexListPl, 
 const sexReplyDeclared = [...reactHow.sexReactionUniversal];
 const helloQueryDeclared = [...reactReplyTo.helloListPl, ...reactReplyTo.helloListUniversal];
 const helloReplyDeclared = [...replyHow.helloRepliesPl];
+const yourMomQueryDeclared = [...reactReplyTo.yourMomListPl];
+const yourMomReplyDeclared = [...replyHow.yourMomRepliesPl];
+const loveQueryDeclared = [...reactReplyTo.loveListPl,...reactReplyTo.loveListEng];
+const loveReplyDeclared = [...replyHow.loveRepliesPl];
 
 
   const curseBot01 = new InteractWith();
   curseBot01.react(msg, sexQueryDeclared, sexReplyDeclared);
   curseBot01.reply(msg, helloQueryDeclared, helloReplyDeclared);
+  curseBot01.replyTag(msg, yourMomQueryDeclared, yourMomReplyDeclared);
+  curseBot01.replyTag(msg, loveQueryDeclared, loveReplyDeclared);
 });
 
 client.login('ODM1NTc3MTIwMjE0MDg5Nzc5.YIRd1Q.k-pDUvnJEfvjUbuQJbZSMp8PwmI');
